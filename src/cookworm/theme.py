50fccbb174db6266bc7ac05f48a077e7f90bd75e
Wilbur Jaywright <zargulthewizard@proton.me>
2026-04-29 12:20:27 -0400

    Moved comments to variable docstrings, and improved them

--- src/cookworm/theme.py
+++ src/cookworm/theme.py
@@ -20,7 +20,6 @@ S.D.G"""
 
 import tkinter as tk
 
-# Styling colors
 COLORS = {
     "leather": "#945110",
     "dark_leather": "#732c08",
@@ -33,28 +32,28 @@ COLORS = {
     "dark_char": "#202020",
     "link_blue": "#00f",
     "sapphire": "#2dc3ff",
-    }
+}
+"""Styling colors"""
 
-# Buttons and such
 LEATHER_STYLE_MAP = {
     "background": [("!disabled", COLORS["leather"]),
                    ("disabled", COLORS["dark_char"])],
     "foreground": [("active", COLORS["gold"]),
                    ("!disabled", "black"),
                    ("disabled", COLORS["char"])],
-    }
+}
+"""Mainly for buttons and other similar widgets"""
 
-# Tiles
 PAPER_STYLE_MAP = {
     "fieldbackground": [  # ("focus", COLORS["selected_paper"]),
-                        ("!disabled", COLORS["selected_paper"]),
-                        ("disabled", COLORS["dark_char"])],
+        ("!disabled", COLORS["selected_paper"]),
+        ("disabled", COLORS["dark_char"])],
     "foreground": [  # ("selected", "focus", "white"),
-                   ("!disabled", "black"),
-                   ("disabled", COLORS["char"])],
+        ("!disabled", "black"),
+        ("disabled", COLORS["char"])],
 }
+"""Mimicking tiles in the game, so for the word list"""
 
-# The overall GUI theme
 LIBRARY_THEME = {
     "TFrame": {"map": LEATHER_STYLE_MAP},
     "TScrollbar": {
@@ -63,27 +62,27 @@ LIBRARY_THEME = {
             "troughcolor": [
                 ("!disabled", COLORS["dark_leather"]),
                 ("disabled", COLORS["dark_char"]),
-                ],
-            },
+            ],
         },
+    },
     "TProgressbar": {
         "map": {
             "troughcolor": [
                 ("!disabled", COLORS["leather"]),
                 ("disabled", COLORS["dark_char"]),
-                ],
-            }
-        },
+            ],
+        }
+    },
     "TButton": {
         "map": {
             **LEATHER_STYLE_MAP,
             "relief": [
                 ("!pressed", "raised"),
                 ("pressed", "sunken"),
-                ],
+            ],
             "anchor": tk.CENTER,
-            }
-        },
+        }
+    },
     "TEntry": {"map": PAPER_STYLE_MAP},
     "TLabel": {"map": LEATHER_STYLE_MAP},
     "Score.TLabel": {
@@ -91,41 +90,41 @@ LIBRARY_THEME = {
             "background": [
                 ("!disabled", COLORS["gold"]),
                 ("disabled", COLORS["dark_char"]),
-                ],
+            ],
             "foreground": [
                 ("!disabled", "black"),
                 ("disabled", COLORS["char"]),
-                ],
-            }
-        },
+            ],
+        }
+    },
     "WordDisplay.TLabel": {
         "map": {
             "background": [
                 ("!disabled", COLORS["entry_wall"]),
                 ("disabled", COLORS["dark_char"]),
-                ],
+            ],
             "foreground": [
                 ("!disabled", COLORS["selected_paper"]),
                 ("disabled", COLORS["char"]),
-                ],
-            }
-        },
+            ],
+        }
+    },
     "Link.TLabel": {
         "map": {
             "foreground": [
                 ("!disabled", COLORS["link_blue"]),
                 ("disabled", COLORS["char"]),
-                ],
-            }
+            ],
         }
     }
+}
+"""The overall GUI theme, to look like the BookWorm library"""
 
-
-# Menubar parameters dict
 MENU_STYLING_KWARGS = {
     "bg": COLORS["dark_leather"],
     "fg": "white",
     "activebackground": COLORS["leather"],
     "activeforeground": COLORS["gold"],
     "disabledforeground": COLORS["char"],
-    }
+}
+"""Styling parameters for the menu widgets, since they are not TTk"""
